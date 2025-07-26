41050abdae49143821080fdd2aad70f376e1b016
Jose Salazar <jmsalazar.macg@hotmail.com>
2025-07-25 23:37:17 -0600

    Dice.cs refactor to improve speed

--- Utils/Dice.cs
+++ Utils/Dice.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiceGame.Utils;
 
@@ -7,11 +8,7 @@ public readonly struct Die
 {
     public int[] Faces { get; }
     public int FaceCount => Faces.Length;
-    public Die(int[] faces)
-    {
-        Faces = faces;
-    }
-
+    public Die(int[] faces) => Faces = faces;
     public int GetFaceValue(int index) => Faces[index];
 }
 
@@ -19,28 +16,20 @@ public static class DiceParser
 {
     public static List<Die> ParseDice(ReadOnlySpan<string> args)
     {
-        if (args.Length < 3)
-        {
-            throw new ArgumentException("At least 3 dice must be provided. Example: 1,2,3,4,5,6 7,8,9,10,11,12");
-        }
+        if (args.Length < 3) throw new ArgumentException("At least 3 dice must be provided. Example: 1,2,3,4,5,6 7,8,9,10,11,12");
 
         var dice = new List<Die>(args.Length);
-        // This is for setting the faces of the first die to match the next ones
-        int expectedFaceCount = -1;
+        int? expectedFaceCount = null;
 
         foreach (var arg in args)
         {
-            var faceValues = ParseDieString(arg);
-            if (expectedFaceCount == -1)
-            {
-                expectedFaceCount = faceValues.Length;
-            }
-            else if (faceValues.Length != expectedFaceCount)
-            {
-                throw new ArgumentException($"This is a fair dice game, so all dice must have the same number of faces. Expected {expectedFaceCount} faces, but got {faceValues.Length} in: {arg}");
-            }
+            var faces = ParseDieString(arg);
+            expectedFaceCount ??= faces.Length;
 
-            dice.Add(new Die(faceValues));
+            if (faces.Length != expectedFaceCount)
+                throw new ArgumentException($"This is a fair dice game, so all dice must have the same number of faces. Expected {expectedFaceCount} faces, but got {faces.Length} in: {arg}");
+
+            dice.Add(new Die(faces));
         }
 
         return dice;
@@ -48,15 +37,10 @@ public static class DiceParser
 
     private static int[] ParseDieString(string dieString)
     {
-        var segments = dieString.Split(',');
-        var faces = new int[segments.Length];
-
-        for (int i = 0; i < segments.Length; i++)
-        {
-            if (!int.TryParse(segments[i].Trim(), out int val) || val < 0)
-                throw new ArgumentException($"Invalid face value: '{segments[i]}'. Must be non-negative *integer* and can't be a letter");
-            faces[i] = val;
-        }
-        return faces;
+        return dieString.Split(',')
+            .Select(s => int.TryParse(s.Trim(), out int val) && val >= 0
+                ? val
+                : throw new ArgumentException($"Invalid face value: '{s}'. Must be non-negative *integer* and can't be a letter"))
+            .ToArray();
     }
 }
